ae46662f92b247f0885d88b60908939b1f0821b3
ShAlIow <99378468+ShAlIow@users.noreply.github.com>
2024-06-25 01:33:58 +0800

    Update Footer.js

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -1,7 +1,16 @@
 import { siteConfig } from '@/lib/config'
 import DarkModeButton from '@/components/DarkModeButton'
-
+import { useEffect } from 'react';
 const Footer = ({ title }) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/path/to/timeCounter.js"; // 替换成实际的路径
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   const d = new Date()
   const currentYear = d.getFullYear()
   const since = siteConfig('SINCE')
@@ -29,22 +38,6 @@ const Footer = ({ title }) => {
                 {/* 加入你想要添加的计时器脚本 */}
                 <span id="timeDate">载入天数...</span>
                 <span id="times">载入时分秒...</span>
-                <script>
-                  var now = new Date();
-                  function createtime() {
-                    var grt= new Date("25/06/2024 16:44:00"); // 此处修改你的建站时间或者网站上线时间
-                    now.setTime(now.getTime() + 250);
-                    days = (now - grt) / 1000 / 60 / 60 / 24; dnum = Math.floor(days);
-                    hours = (now - grt) / 1000 / 60 / 60 - (24 * dnum); hnum = Math.floor(hours);
-                    if (String(hnum).length == 1) { hnum = "0" + hnum; } minutes = (now - grt) / 1000 / 60 - (24 * 60 * dnum) - (60 * hnum);
-                    mnum = Math.floor(minutes); if (String(mnum).length == 1) { mnum = "0" + mnum; }
-                    seconds = (now - grt) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
-                    snum = Math.round(seconds); if (String(snum).length == 1) { snum = "0" + snum; }
-                    document.getElementById("timeDate").innerHTML = "本站已安全运行 " + dnum + " 天 ";
-                    document.getElementById("times").innerHTML = hnum + " 小时 " + mnum + " 分 " + snum + " 秒";
-                  }
-                  setInterval(createtime, 250);
-                </script>
             </span>
         </footer>
   )
