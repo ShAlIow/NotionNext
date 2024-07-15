567ec69d5ea5918201a6f621905a8c27ebeaafca
ShAlIow <99378468+ShAlIow@users.noreply.github.com>
2024-07-15 14:59:23 +0800

    Update Footer.js

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -19,11 +19,12 @@ const Footer = ({ title }) => {
 
                 {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a><br /></>}
                 <a href="https://icp.gov.moe/?keyword=20245918" target="_blank">萌ICP备20245918号</a>
-                <script defer src="https://vercount.one/js"></script>
+                <div>
                 <span className='hidden busuanzi_container_site_pv'>
                     <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span> </span>
                 <span className='pl-2 hidden busuanzi_container_site_uv'>
                     <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
+                </div>
                 <br />
                 <h1>{/* 加入你想要添加的计时器脚本 */}
                 <span id="timeDate">载入天数...</span>
